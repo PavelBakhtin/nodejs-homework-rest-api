6671906e8f1a30088bd0a4b8f0e4a188ad850636
PavelBakhtin <pavelbakhtin88@gmail.com>
2023-01-15 16:56:56 +0200

    1

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,54 +1,82 @@
-const {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-} = require("../models/contacts.js");
 const { HttpError } = require("../utilities/index");
-
+const { Contact } = require("../models/contacts.js");
 const getContacts = async (req, res, next) => {
-  const contactsList = await listContacts();
-  res.status(200).json(contactsList);
+  const { limit } = req.query;
+  const contacts = await Contact.find({}).limit(limit);
+  res.status(200).json(contacts);
 };
 
 const getContact = async (req, res, next) => {
   const { id } = req.params;
-  const contact = await getContactById(id);
+  const contact = await Contact.findById(id);
+  if (!contact) {
+    return next(HttpError(404, "Not found"));
+  }
   return res.status(200).json(contact);
 };
 
 const createContact = async (req, res, next) => {
-  const newContact = await addContact(req.body);
+  const { name, phone, email, favorite } = req.body;
+  const newContact = await Contact.create({ name, phone, email, favorite });
   res.status(201).json(newContact);
 };
 
 const deleteContact = async (req, res, next) => {
   const { id } = req.params;
-  const contact = await getContactById(id);
+  const contact = await Contact.findById(id);
   if (!contact) {
     return next(HttpError(404, "Not found"));
   }
-  await removeContact(id);
+  await Contact.findByIdAndRemove(id);
   res.status(200).json({ message: "contact deleted" });
 };
+
 const changeContact = async (req, res, next) => {
+  const { name, phone, email } = req.body;
   const { id } = req.params;
-  const contact = await getContactById(id);
+  const contact = await Contact.findById(id);
   if (!req.body) {
     return res.status(400).json({ message: "missing fields" });
   }
   if (!contact) {
     return next(HttpError(404, "Not found"));
   }
-  const updatedContact = await updateContact(id, req.body);
+  const updatedContact = await Contact.findByIdAndUpdate(
+    id,
+    {
+      $set: { name, phone, email },
+    },
+    { new: true }
+  );
+
   res.status(200).json(updatedContact);
 };
 
+const updateStatusContact = async (req, res, next) => {
+  const { favorite } = req.body;
+  const { id } = req.params;
+  const contact = await Contact.findById(id);
+  if (!req.body) {
+    return res.status(400).json({ message: "missing fields favorite" });
+  }
+  if (!contact) {
+    return next(HttpError(404, "Not found"));
+  }
+  const updatedContact = await Contact.findByIdAndUpdate(
+    id,
+    {
+      $set: { favorite },
+    },
+    { new: true }
+  );
+
+  res.status(200).json(updatedContact);
+};
 module.exports = {
   getContacts,
   changeContact,
   deleteContact,
   createContact,
   getContact,
+  updateStatusContact,
 };
--- server.js
+++ server.js
@@ -1,5 +1,19 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+mongoose.set("strictQuery", false);
+const { HOST_URI } = process.env;
+(async () => {
+  app.listen(3000, () => {
+    console.log("Server running. Use our API on port: 3000");
+  });
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+  try {
+    await mongoose.connect(HOST_URI);
+    console.log("Database connection successful");
+  } catch (error) {
+    console.error("Error while connecting to database", error.massage);
+    process.exit(1);
+  }
+})();
